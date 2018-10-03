f36ddefb83b219039abc56d147f2fbfa0d07023f
simont1 <stsui2@stuy.edu>
2018-10-03 10:58:12 -0400

    iogj

--- clas.c
+++ clas.c
@@ -3,41 +3,49 @@
 #include <time.h>
 
 struct pokemon {char * name; int hp;};
+struct pokemon pikachu;
+
 
 int healthgen(){
-  srand(time(NULL));
   int x = rand();
-  x = x % 100;
+  x = (x % 90) + 10;
   return x;
 }
 
-char* namegen(){
+char* namegen(char *s){
   char a[10] = "aaaaa";
   int ctr = 0;
   while(ctr < 5){
     char x = rand();
-    printf("%d\n", x);
+    printf("old: %d\n", x);
     x = (x % 26) + 65;
+    printf("new: %d\n", x);
     *(a+ctr) = x;
     ctr++;
     printf("%d\n", x);
   }
   printf("%s\n", a);
-  return a;
+  *s = *a;
+  return s;
 }
 
 struct pokemon ditto(){
-  struct pokemon *pikachu;
-  pikachu->name = namegen();
-  pikachu->hp = healthgen();
-  printf("%s\n", pikachu->name);
+  pikachu.name = namegen(pikachu.name);
+  pikachu.hp = healthgen();
+  return pikachu;
 }
 
 
 
+struct pokemon reachStats(){
+  printf("%d\n", pikachu.hp);
+  printf("%s\n", pikachu.name);
+}
 
 int main(){
+  srand(time(NULL));
   ditto();
+  reachStats();
   return 0;
   
 }
